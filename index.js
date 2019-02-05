10d1f07c03cdb9e47b3ff67081b704d1587111c9
Teerapong Dinon <tp.dinon@gmail.com>
2019-02-05 21:57:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function takeANumber(katzDeli, name){
 function nowServing(katzDeliLine){
   if(katzDeliLine.length > 0){
     var nowServing = katzDeliLine[0]
-    return 
+    
+    return `Now serving ${katzDeliLine}
   }katzDeliLine.shift(katzDeliLine)
   return `"There is nobody waiting to be served!"`
 }
