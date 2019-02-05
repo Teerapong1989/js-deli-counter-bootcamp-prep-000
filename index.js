3ccdd052d58182d460f06c8e9336b1d6ba01fe10
Teerapong Dinon <tp.dinon@gmail.com>
2019-02-05 22:05:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function currentLine(line){
     for(let i = 0; i < line.length; i++){
       array.push(` ${i+1}. ${line[i]}`);
     }
-    return `""The line is currently: " + array`
+    return `""The line is currently: "` + array
   }
 }
