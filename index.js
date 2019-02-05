7567ceb21907cc76d06712557fe5efeaa2dfba99
Teerapong Dinon <tp.dinon@gmail.com>
2019-02-05 22:03:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ function currentLine(line){
   if(line.length === 0){
     return `"The line is currently empty."`
   } else {
-    
+    var arry = [];
+    for(let i = 0; i < line.length; i++){
+      retune 
+    }
   }
 }
